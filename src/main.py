a4c3549ff42f571e73aae57b9f218b41a9ca5139
Matthew Concepcion <matthewconcepcion10@gmail.com>
2024-11-01 13:01:45 +0800

    fix: add middleware

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import random
 import json
 
 app = FastAPI()
+
+# Add CORS middleware
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "https://qotd-site.netlify.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 with open('data/questions.json', 'r') as file:
     questions = json.load(file)
