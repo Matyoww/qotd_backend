edc5c0cc05cbc47175bb12eb4b0f63d8ce2291b7
Matthew Concepcion <matthewconcepcion10@gmail.com>
2024-11-14 01:39:55 +0800

    feat: added post, put, and delete method

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from mongodb_user import mongoDBManager
 import random
-import json
 
 app = FastAPI()
+db_mgr = mongoDBManager()
 
 # Add CORS middleware
 origins = [
@@ -21,21 +22,42 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-with open('data/questions.json', 'r') as file:
-    questions = json.load(file)
-
 @app.get("/question", tags=["question"])
 def read_question():
-    q = random.choice(questions["questions"])
-    return q["question"]
+    questions = db_mgr.get_all_questions()
+    questions = [question["question"] for question in questions]
+    random_question = random.choice(questions)
+    return random_question
+
+@app.post("/question", tags=["question"])
+def add_question(category: str, question: str):
+    db_mgr.create_question(category, question)
+    return {"status": "Question added successfully"}
 
 @app.get("/question/all", tags=["question"])
 def read_all_questions(include_meta: bool = True):
+    questions = db_mgr.get_all_questions()
     if include_meta:
         return questions
     else:
-        return {"questions": [q["question"] for q in questions["questions"]]}
+        return [question["question"] for question in questions]
 
 @app.get("/question/{question_id}", tags=["question"])
-def read_question_by_id(question_id: int):
-    return questions["questions"][question_id - 1]["question"]
+def read_question_by_id(question_id: int, include_meta: bool = True):
+    question = db_mgr.collection.find_one({"qid": question_id})
+    question["_id"] = str(question["_id"])
+
+    if include_meta:
+        return question
+    else:
+        return question["question"]
+
+@app.put("/question/{question_id}", tags=["question"])
+def update_question(question_id: int, question: str):
+    db_mgr.update_question(question_id, question)
+    return {"status": "Question updated successfully"}
+
+@app.delete("/question/{question_id}", tags=["question"])
+def delete_question(question_id: int):
+    db_mgr.delete_question(question_id)
+    return {"status": "Question deleted successfully"}
---
+++ src/mongodb_user.py
@@ -0,0 +1,53 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+mongodb_user = os.getenv("MONGODB_USER")
+mongodb_pass = os.getenv("MONGODB_PASS")
+
+uri = f"mongodb+srv://{mongodb_user}:{mongodb_pass}@qotdcluster.lxdq3.mongodb.net/?retryWrites=true&w=majority&appName=qotdCluster"
+
+class mongoDBManager:
+    def __init__(self):
+        self.client = MongoClient(uri, server_api=ServerApi('1'))
+        self.db = self.client['qotd']
+        self.collection = self.db['questions']
+
+    def get_all_questions(self):
+        questions = list(self.collection.find({}))
+        for question in questions:
+            question["_id"] = str(question["_id"])
+        return questions
+
+    def create_question(self, category: str, question: str):
+        qid_collection = self.db['qid_sequence']
+        id_counter = qid_collection.find_one_and_update(
+            {"_id" : "qid_seq"},
+            {"$inc" : {"qid_counter" : 1}},
+            upsert=True,
+            return_document=True
+        )
+        self.collection.insert_one({
+            "qid" : id_counter['qid_counter'],
+            "category" : category,
+            "question": question}
+        )
+
+    def update_question(self, question_id: int, question: str):
+        self.collection.update_one({"qid": question_id}, {"$set": {"question": question}})
+
+    def delete_question(self, question_id: int):
+        self.collection.delete_one({"qid": question_id})
+
+    def get_number_of_questions(self):
+        return self.collection.count_documents({})
+
+    def ping(self):
+        try:
+            self.client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
